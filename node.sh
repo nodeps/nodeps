16d72cc26ff64c3ca250c8475a636164694865f6
bruce.wang <37991842@qq.com>
2017-12-28 16:39:49 +0800

    Update node.sh

--- node.sh
+++ node.sh
@@ -2,7 +2,6 @@
 node_version="8.9.3"
 # 下载node包
 wget https://nodejs.org/dist/v${node_version}/node-v${node_version}-linux-x64.tar.xz
-#wget http://192.168.31.202:8000/node-v${node_version}-linux-x64.tar.xz
   
 # 解压二进制源码
 xz -d node-v${node_version}-linux-x64.tar.xz
@@ -15,8 +14,4 @@ echo 'export NODE_HOME=/root/node-v'${node_version}'-linux-x64'>>/etc/profile
 echo 'export PATH=$NODE_HOME/bin:$PATH'>>/etc/profile
 source /etc/profile
 
-exit 0
-
-#sh -c "$(curl -fsSL http://gitlab.tools.vipshop.com/bruce01.wang/node-install-linux/blob/master/node.sh)"
-#sh -c "$(curl -fsSL http://192.168.31.202:8000/node.sh)"
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/nodeps/nodeps/master/node.sh)"
